5e80566a40f02d263718600ff3573385ea22cd9c
Uddhav-Mishra <f2014048@hyderabad.bits-pilani.ac.in>
2017-08-20 15:21:32 +0530

    calculator src added

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,386 @@
+package Calculator;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Window.Type;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import java.awt.Font;
+import java.awt.event.InputMethodListener;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+
+/**
+ * Class for the calculator application.
+ */
+public class Calculator {
+
+	private JFrame frame;
+	private JTextField expression;
+	private JTable table;
+	private JButton clearbtn;
+	private JButton flipsignbtn;
+	private JButton button_1;
+	private JButton dividebtn;
+	private JButton sevenbtn;
+	private JButton eightbtn;
+	private JButton ninebtn;
+	private JButton probtn;
+	private JButton fourbtn;
+	private JButton fivebtn;
+	private JButton sixbtn;
+	private JButton minusbtn;
+	private JButton threebtn;
+	private JButton twobtn;
+	private JButton onebtn;
+	private JButton plusbtn;
+	private JButton zerobtn;
+	private JButton decibtn;
+	private JButton base2btn;
+	private JTextField result;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Calculator window = new Calculator();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Calculator() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setType(Type.UTILITY);
+		frame.setBounds(100, 100, 229, 342);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		expression = new JTextField();
+		expression.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		
+		expression.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+			//	System.out.println("");
+			}
+			
+		});
+		expression.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			  public void changedUpdate(DocumentEvent e) {
+					//System.out.println("regrg");
+
+			  }
+
+			@Override
+			public void insertUpdate(DocumentEvent arg0) {
+			System.out.println(expression.getText());
+				String s = EvaluateString.evaluate(expression.getText());
+				if(s != "invalid")
+					result.setText(s);
+					
+				
+			}
+
+			@Override
+			public void removeUpdate(DocumentEvent arg0) {
+			//	System.out.println(expression.getText());
+				//String s = EvaluateString.evaluate(expression.getText());
+				//if(s != "invalid")
+					//result.setText(s);
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		expression.addInputMethodListener(new InputMethodListener() {
+			public void caretPositionChanged(InputMethodEvent arg0) {
+			}
+			public void inputMethodTextChanged(InputMethodEvent arg0) {
+				System.out.println("loda");
+			}
+		});
+		expression.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		expression.setBounds(0, 0, 213, 49);
+		frame.getContentPane().add(expression);
+		expression.setColumns(10);
+		
+		table = new JTable();
+		table.setFillsViewportHeight(true);
+		table.setColumnSelectionAllowed(true);
+		table.setCellSelectionEnabled(true);
+		table.setBounds(0, 263, 219, -195);
+		frame.getContentPane().add(table);
+		
+		clearbtn = new JButton("AC");
+		clearbtn.setForeground(Color.RED);
+		clearbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				expression.setText("");
+				result.setText("");
+				
+			}
+		});
+		clearbtn.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		clearbtn.setBounds(0, 67, 54, 49);
+		frame.getContentPane().add(clearbtn);
+		
+		flipsignbtn = new JButton(".");
+		flipsignbtn.setBackground(UIManager.getColor("Button.background"));
+		flipsignbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'.');
+
+				
+			}
+		});
+		flipsignbtn.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		flipsignbtn.setBounds(53, 68, 54, 49);
+		frame.getContentPane().add(flipsignbtn);
+		
+		button_1 = new JButton("%");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+button_1.getText());
+				
+			}
+		});
+		button_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		button_1.setBounds(106, 67, 54, 49);
+		frame.getContentPane().add(button_1);
+		
+		dividebtn = new JButton("/");
+		dividebtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'/');
+
+				
+			}
+		});
+		dividebtn.setFont(new Font("Tahoma", Font.BOLD, 12));
+		dividebtn.setBackground(Color.ORANGE);
+		dividebtn.setBounds(159, 67, 54, 49);
+		frame.getContentPane().add(dividebtn);
+		
+		sevenbtn = new JButton("7");
+		sevenbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'7');
+
+			}
+		});
+		sevenbtn.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		sevenbtn.setBounds(0, 115, 54, 49);
+		frame.getContentPane().add(sevenbtn);
+		
+		eightbtn = new JButton("8");
+		eightbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'8');
+
+			}
+		});
+		eightbtn.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		eightbtn.setBounds(53, 115, 54, 49);
+		frame.getContentPane().add(eightbtn);
+		
+		ninebtn = new JButton("9");
+		ninebtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'9');
+
+			}
+		});
+		ninebtn.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		ninebtn.setForeground(Color.BLACK);
+		ninebtn.setBounds(106, 115, 54, 49);
+		frame.getContentPane().add(ninebtn);
+		
+		probtn = new JButton("*");
+		probtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'*');
+
+			}
+		});
+		probtn.setFont(new Font("Tahoma", Font.BOLD, 17));
+		probtn.setForeground(Color.BLACK);
+		probtn.setBackground(Color.ORANGE);
+		probtn.setBounds(159, 115, 54, 49);
+		frame.getContentPane().add(probtn);
+		
+		fourbtn = new JButton("4");
+		fourbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'4');
+
+			}
+		});
+		fourbtn.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		fourbtn.setBounds(0, 163, 54, 49);
+		frame.getContentPane().add(fourbtn);
+		
+		fivebtn = new JButton("5");
+		fivebtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'5');
+
+			}
+		});
+		fivebtn.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		fivebtn.setBounds(53, 163, 54, 49);
+		frame.getContentPane().add(fivebtn);
+		
+		sixbtn = new JButton("6");
+		sixbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'6');
+
+			}
+		});
+		sixbtn.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		sixbtn.setBounds(106, 163, 54, 49);
+		frame.getContentPane().add(sixbtn);
+		
+		minusbtn = new JButton("-");
+		minusbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'-');
+
+			}
+		});
+		minusbtn.setFont(new Font("Tahoma", Font.BOLD, 17));
+		minusbtn.setBackground(Color.ORANGE);
+		minusbtn.setBounds(159, 163, 54, 49);
+		frame.getContentPane().add(minusbtn);
+		
+		threebtn = new JButton("3");
+		threebtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'3');
+
+			}
+		});
+		threebtn.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		threebtn.setBounds(106, 211, 54, 49);
+		frame.getContentPane().add(threebtn);
+		
+		twobtn = new JButton("2");
+		twobtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'2');
+
+			}
+		});
+		twobtn.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		twobtn.setBounds(53, 211, 54, 49);
+		frame.getContentPane().add(twobtn);
+		
+		onebtn = new JButton("1");
+		onebtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'1');
+
+			}
+		});
+		onebtn.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		onebtn.setBounds(0, 211, 54, 49);
+		frame.getContentPane().add(onebtn);
+		
+		plusbtn = new JButton("+");
+		plusbtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'+');
+
+			}
+		});
+		plusbtn.setFont(new Font("Tahoma", Font.BOLD, 17));
+		plusbtn.setBackground(Color.ORANGE);
+		plusbtn.setBounds(159, 211, 54, 49);
+		frame.getContentPane().add(plusbtn);
+		
+		zerobtn = new JButton("0");
+		zerobtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'0');
+
+			}
+		});
+		zerobtn.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		zerobtn.setBounds(0, 258, 107, 49);
+		frame.getContentPane().add(zerobtn);
+		
+		decibtn = new JButton("(");
+		decibtn.setBackground(Color.YELLOW);
+		decibtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+'(');
+
+			}
+		});
+		decibtn.setFont(new Font("Tahoma", Font.BOLD, 17));
+		decibtn.setBounds(106, 258, 54, 49);
+		frame.getContentPane().add(decibtn);
+		
+		base2btn = new JButton(")");
+		base2btn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				expression.setText(expression.getText()+')');
+
+			}
+		});
+		base2btn.setFont(new Font("Tahoma", Font.BOLD, 17));
+		base2btn.setBackground(Color.YELLOW);
+		base2btn.setBounds(159, 258, 54, 49);
+		frame.getContentPane().add(base2btn);
+		
+		result = new JTextField();
+		result.setHorizontalAlignment(SwingConstants.RIGHT);
+		result.setFont(new Font("Tahoma", Font.BOLD, 11));
+		result.setEditable(false);
+		result.setBounds(0, 48, 213, 20);
+		frame.getContentPane().add(result);
+		result.setColumns(10);
+	}
+}
---
+++ src/Calculator/EvaluateString.java
@@ -0,0 +1,154 @@
+package Calculator;
+
+import java.util.Stack;
+
+/**
+* Evaluates the value of the given expression string.
+*/ 
+public class EvaluateString {
+    /**
+    * Evaluates the value of the given expression string.
+    */
+    public static String evaluate(String expression){
+        int cnt = 0;
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '(') {
+                cnt++;
+            }
+            else if (expression.charAt(i) == ')') {
+                cnt--;
+            }
+        }
+        for(int i = 0;i < cnt; i++) {
+             expression = expression + ')';
+           
+        }
+        
+        char[] tokens = expression.toCharArray();
+ 
+         // Stack for numbers: 'values'
+        Stack<Double> values = new Stack<Double>();
+ 
+        // Stack for Operators: 'ops'
+        Stack<Character> ops = new Stack<Character>();
+        int index = tokens.length-1;
+        for (int i =  tokens.length-1; i >= 0; i--)
+        { 
+            if (tokens[i] != ' ' && tokens[i] != '(' && tokens[i] != ')') {
+                index = i;
+                break;
+            }
+        }
+        if (!(tokens[index] >= '0' && tokens[index] <='9')) {
+            return "invalid";
+        }
+        for (int i = 0; i < tokens.length; )
+        {   //System.out.println("char length"+tokens[i]);
+             // Current token is a whitespace, skip it
+            if (tokens[i] == ' ')
+            {i++; continue;}
+            
+            double negflag = 1.0;
+                // There may be more than one digits in number
+                if (tokens[i] == '-') {
+                    if (i > 0 ) { 
+                       if (tokens[i-1] == '(') {
+                            System.out.println("negative number");
+                           negflag = -1.0 ;
+                           i++;
+                       }
+                    }        
+                }
+            // Current token is a number, push it to stack for numbers
+            if ((tokens[i] >= '0' && tokens[i] <= '9') || tokens[i] == '.' )
+            {
+                StringBuffer sbuf = new StringBuffer();
+             
+                while ((i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9') || 
+                        (i < tokens.length && tokens[i] == '.'))
+                    sbuf.append(tokens[i++]);
+                values.push((negflag*Double.parseDouble(sbuf.toString())));
+            }
+ 
+            // Current token is an opening brace, push it to 'ops'
+            else if (tokens[i] == '(')
+                ops.push(tokens[i++]);
+ 
+            // Closing brace encountered, solve entire brace
+            else if (tokens[i] == ')')
+            {
+                while (ops.peek() != '(')
+                  values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                ops.pop();
+                i++;
+            }
+ 
+            // Current token is an operator.
+            else if (tokens[i] == '+' || (tokens[i] == '-' && negflag > 0) ||
+                     tokens[i] == '*' || tokens[i] == '/' || tokens[i] == '&' || tokens[i] == '|' 
+                     || tokens[i] == '^' || tokens[i] == '%')
+            {   
+                // While top of 'ops' has same or greater precedence to current
+                // token, which is an operator. Apply operator on top of 'ops'
+                // to top two elements in values stack
+                while (!ops.empty() && hasPrecedence(tokens[i], ops.peek()))
+                  values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+ 
+                // Push current token to 'ops'.
+                ops.push(tokens[i++]);
+            }
+        }
+        System.out.println(ops.size());
+        // Entire expression has been parsed at this point, apply remaining
+        // ops to remaining values
+        while (!ops.empty())
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+ 
+        // Top of 'values' contains result, return it
+        return values.pop().toString();
+    }
+ 
+    /** Returns true if 'op2' has higher or same precedence as 'op1',
+    * otherwise returns false.
+    */
+    public static boolean hasPrecedence(char op1, char op2)
+    {
+        if (op2 == '(' || op2 == ')')
+            return false;
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+            return false;
+        else
+            return true;
+    }
+ 
+    /** A utility method to apply an operator 'op' on operands 'a' 
+    *   and 'b'. Return the result.
+    */
+    public static double applyOp(char op, double b, double a)
+    {
+        switch (op)
+        {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            if (b == 0)
+                throw new
+                UnsupportedOperationException("Cannot divide by zero");
+            return a / b;
+        case '&':
+            return (double)(Math.round(a) & Math.round(b)) ;
+        case '|':
+            return (double)(Math.round(a) | Math.round(b)) ;
+        case '^':
+            return (double)(Math.round(a) ^ Math.round(b)) ;
+        case '%':
+            return (double)(Math.round(a) % Math.round(b)) ;    
+        }
+        return 0;
+     
+    }
+}
